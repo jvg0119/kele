79ca4ef6b9d174c237d022b6e66e1355471e9744
Joseph <jvg_0119@yahoo.com>
2017-08-16 12:12:47 -0700

    2_initialize_authorize

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,26 +1,32 @@
 
 require 'httparty'
 require 'json'
-require 'rest_client'
 
 class Kele
   include HTTParty
 
   base_uri "https://www.bloc.io/api/v1/"
 
-  #attr_accessor :email, :password
-
   def initialize(email, password)
-    #@email = email
-    #@password = password
-    response_body = self.class.post(base_api_endpoint("sessions"), body: {"email": email, "password": password} )
+    response = self.class.post(api_endpoint("sessions"), body: {"email": email, "password": password} )
 
-    puts @auth_token = response_body['auth_token']
+    if (response.code == 200) && response['auth_token']
+      puts "All good!"
+    else
+      puts "Email or password was incorrect"
+    end
+
+    @auth_token = response['auth_token']
+
+      # puts response.code
+      # puts response.message
+      # puts response.body
+      # puts response.code
   end
 
   private
 
-  def base_api_endpoint(end_point)
+  def api_endpoint(end_point)
     "https://www.bloc.io/api/v1/#{end_point}"
   end
 
